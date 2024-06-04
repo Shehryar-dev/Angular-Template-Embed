a8d65ca31450b64d68ce876fd6e431285fc4ab7a
Shehriyar Saleem <sherryop121@gmail.com>
2024-06-04 23:15:37 +0500

    Angular Assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent }, // Default route
+  { path: 'about', component: AboutComponent },
+  { path: 'testimonial', component: TestimonialComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'product', component: ProductComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route for a 404 page
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+import { BlogComponent } from './blog/blog.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductComponent,
+    BlogComponent,
+    TestimonialComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
